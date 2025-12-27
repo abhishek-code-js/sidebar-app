eb82e820309b8f8ccd449483d189b3076cf238ad
Abhishek Shah <abhishekmac@Abhisheks-MacBook-Pro.local>
2025-12-27 16:06:37 +0530

    added logic to toggle sidebar - open and close logic

---
+++ app.js
@@ -0,0 +1,11 @@
+const sidebarToggleBtn = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+sidebarToggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("show-sidebar");
+});
